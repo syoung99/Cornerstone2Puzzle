d59c47eab5754dd4c9adc25591050a2272aa7163
bmchale98 <mchale.b@husky.neu.edu>
2018-03-01 13:12:47 -0500

    Fixed button issue continuously displaying 1's, issue with buttons?

--- ArduinoOutputCode/joystickTest/joystickTest.ino
+++ ArduinoOutputCode/joystickTest/joystickTest.ino
@@ -12,6 +12,10 @@ const int RedBtnPin = 2;
 const int GreenBtnPin = 3;
 const int BlueBtnPin = 4;
 
+bool RedRead = true;
+bool GreenRead = true;
+bool BlueRead = true;
+
 void setup()
 {
   pinMode(RedBtnPin, INPUT);
@@ -26,24 +30,59 @@ void loop()
 {
   xval = map(analogRead(A0),0,1023,-100,100);
   yval = map(analogRead(A1),0,1023,-100,100);
-  /*
+  
   RedBtnState = digitalRead(RedBtnPin);
   GreenBtnState = digitalRead(GreenBtnPin);
   BlueBtnState = digitalRead(BlueBtnPin);
-   */
-  
+   
+  if (!RedRead && RedBtnState)
+  {
+    RedBtnState = 0;
+  }
+  else
+  {
+    RedRead = true;
+  }
+  if (!GreenRead && GreenBtnState)
+  {
+    GreenBtnState = 0;
+  }
+  else
+  {
+    GreenRead = true;
+  }
+  if (!BlueRead && BlueBtnState)
+  {
+    BlueBtnState = 0;
+  }
+  else
+  {
+    BlueRead = true;
+  }
   Serial.print(xval);
   Serial.print("\t");
   Serial.print(yval);
   Serial.print("\t");
-  /*
+  
   Serial.print(RedBtnState);
   Serial.print("\t");
   Serial.print(GreenBtnState);
   Serial.print("\t");
   Serial.print(BlueBtnState);
-  */
+  
   Serial.print("\n");
+  if (RedBtnState)
+  {
+    RedRead = false;
+  }
+  if (GreenBtnState)
+  {
+    GreenRead = false;
+  }
+  if (BlueBtnState)
+  {
+    BlueRead = false;
+  }
   delay(100);
 }
 
